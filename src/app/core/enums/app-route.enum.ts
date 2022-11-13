d983643f78e5a8bb487be497665ee7bbdf43c8ff
Viacheslav-ua <atph@ukr.net>
2022-11-13 21:35:51 +0200

    Добавил Enum маршрутов

---
+++ src/app/core/enums/app-route.enum.ts
@@ -0,0 +1,8 @@
+export enum AppRouteEnum {
+  Contacts = 'contacts',
+  ContactsView = 'contacts/view',
+  ContactsEdit = 'contacts/edit',
+  User = 'user',
+  Login = 'login',
+  Registration = 'registration',
+}
