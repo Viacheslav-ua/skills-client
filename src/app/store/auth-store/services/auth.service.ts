062741f4be847465e71e7a3a9e644841db1a1550
Viacheslav-ua <atph@ukr.net>
2022-11-18 05:46:59 +0200

    Добавил http запрос login

--- src/app/store/auth-store/services/auth.service.ts
+++ src/app/store/auth-store/services/auth.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(
+  constructor(private httpClient: HttpClient) { }
 
-  ) { }
+  login(body: { login: string, password: string }) {
+    return this.httpClient.post<{accessToken: string}>('http://localhost:3000/auth/login', body)
+  }
 }
