63606788be97e427875a67a1f6748566d93840ad
Viacheslav-ua <atph@ukr.net>
2022-11-18 06:09:43 +0200

    Доработка эффекта Login

--- src/app/store/auth-store/auth-store.module.ts
+++ src/app/store/auth-store/auth-store.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { StoreModule } from '@ngrx/store'
 import { AuthReducer, AUTH_FEATURE_NAME } from './store/auth-store.reducer'
+import { EffectsModule } from '@ngrx/effects'
+import { AuthEffects } from './store/auth-store.effects'
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    HttpClientModule,
     StoreModule.forFeature(AUTH_FEATURE_NAME, AuthReducer),
+    EffectsModule.forFeature([AuthEffects])
   ]
 })
 export class AuthStoreModule { }
--- src/app/store/auth-store/store/auth-store.effects.ts
+++ src/app/store/auth-store/store/auth-store.effects.ts
@@ -1,19 +1,26 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { of, switchMap } from "rxjs";
+import { map, of, switchMap } from "rxjs";
+import { AuthService } from "../services/auth.service";
 import { login, loginSuccess } from "./auth-store.actions";
 
 @Injectable()
-export class AdminAuthEffects {
+export class AuthEffects {
 
   login$ = createEffect(() => this.actions$.pipe(
     ofType(login),
-    switchMap(action => of(loginSuccess({
-      accessToken: 'test'
-    })))
+    switchMap(action => this.authService.login({
+      login: action.login,
+      password: action.password,
+    }).pipe(
+      map(loginSuccessData => loginSuccess(loginSuccessData))
+    ))
   ))
 
-  constructor(private actions$: Actions) { }
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService,
+  ) { }
 
 
 }
