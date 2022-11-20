6681a436e6a8da43861b1f5b9a4bcf64ca92fd59
Viacheslav-ua <atph@ukr.net>
2022-11-20 13:29:27 +0200

    Внедряем в AuthInterceptor authService, проверка и добавление токена в Headers

--- src/app/store/auth-store/interceptors/auth.interceptor.ts
+++ src/app/store/auth-store/interceptors/auth.interceptor.ts
@@ -6,18 +6,24 @@ import {
   HttpInterceptor
 } from '@angular/common/http'
 import { Observable } from 'rxjs'
+import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler,
   ): Observable<HttpEvent<unknown>> {
-    console.log('auth interceptor');
-
+    if (this.authService.accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${this.authService.accessToken}`
+        }
+      })
+    }
 
     return next.handle(request)
   }
