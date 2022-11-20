6487daa92d2394bdcf5d306e582da30ecb2fe953
Viacheslav-ua <atph@ukr.net>
2022-11-21 00:46:11 +0200

    Добавил реализацию замены токена в AuthEffects

--- src/app/store/auth-store/store/auth-store.effects.ts
+++ src/app/store/auth-store/store/auth-store.effects.ts
@@ -1,8 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { map, of, switchMap } from "rxjs";
+import { select, Store } from "@ngrx/store";
+import { delayWhen, first, map, filter, switchMap, timer } from "rxjs";
 import { AuthService } from "../services/auth.service";
 import { login, loginSuccess } from "./auth-store.actions";
+import { AuthData } from "./auth-store.reducer";
+import { isAuth } from "./auth-store.selectors";
 
 @Injectable()
 export class AuthEffects {
@@ -17,9 +20,25 @@ export class AuthEffects {
     ))
   ))
 
+  refresh$ = createEffect(() => this.actions$.pipe(
+    ofType(loginSuccess),
+    delayWhen((action: AuthData) => timer(
+      action.exp*1000 - 60*1000 - Date.now()
+    )),
+    switchMap(() => this.store.pipe(
+      select(isAuth),
+      first(),
+      filter(authActs => authActs),
+    )),
+    switchMap(() => this.authService.refresh().pipe(
+      map(loginSuccessData => loginSuccess(loginSuccessData)),
+    ))
+  ))
+
   constructor(
     private actions$: Actions,
     private authService: AuthService,
+    private store: Store,
   ) { }
 
 
