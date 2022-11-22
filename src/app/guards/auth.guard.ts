f84860e8695e4c52e0e0148e4d36baa522ce0441
Viacheslav-ua <atph@ukr.net>
2022-11-22 09:52:33 +0200

    Добавил редирект в AuthGuard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -4,22 +4,39 @@ import {
   CanActivate,
   CanLoad,
   Route,
+  Router,
   RouterStateSnapshot,
   UrlSegment,
   UrlTree
 } from '@angular/router'
-import { Observable } from 'rxjs'
+import { first, map, Observable, of } from 'rxjs'
+import { AppRouteEnum } from '../core/enums'
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return confirm('(canActivate). Are you registered?')
+    return this.getIsAuth()
   }
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return confirm('(canLoad). Are you registered?')
+    return this.getIsAuth()
+  }
+
+  private getIsAuth(): Observable<boolean> {
+    return of(true).pipe(
+      first(),
+      map(isAuth => {
+        if (!isAuth) {
+          this.router.navigateByUrl(AppRouteEnum.Login)
+        }
+        return isAuth
+      }),
+
+    )
   }
 }
